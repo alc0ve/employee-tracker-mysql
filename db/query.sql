f6089cd041ff5699d3502d069899c14884e4a319
Christina Hall <cmcclai26@gmail.com>
2023-03-21 20:26:39 -0400

    added all roles fx

--- db/query.sql
+++ db/query.sql
@@ -1,3 +1,11 @@
-SELECT * FROM department
+--view all departments; changes dep_name to display as name--
 SELECT id, dep_name AS 'name' FROM department
 
+--view all roles--
+SELECT id, title, department_id AS 'department', salary FROM roles
+
+--addEmployee function--
+--show role and id--
+SELECT id, title FROM roles
+--manager id and name--
+SELECT manager_id, first_name, last_name FROM employees
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const inquirer = require('inquirer');
 const express = require('express');
 const mySQL = require('mysql2');
 const cTable = require('console.table');
+const colors = require('colors');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -15,51 +16,129 @@ const db = mySQL.createConnection(
         password: 'Password1',
         database: 'employee_db'
     },
-    console.log(`Connected to the employee_db database.`)
+    console.log(`Connected to the employee_db database.`.brightGreen)
 );
 
-//The function will return a Promise that will resolve with the result of the function when it completes.
-const viewAllDepartments = async () => {
-    const data = await new Promise ((resolve, reject) => {
-    db.query('SELECT * FROM department', (err, results) => {
-        if (err) {
-            reject(err)
-        } else {
-            resolve(results)
-        }
-    });
-});
-console.table(data);
-};
+//View All Employees
+
 
 //Add Employee
 const addEmployee = () => {
     // const data = await new Promise (())
     //first name, lastname, role, manager
-    // inquirer.prompt
     //show role and id to pick
-      db.query('SELECT id, title FROM roles', (err, results) => {
+    db.query("SELECT id, title FROM roles", (err, results) => {
         if (err) {
-            reject (err)
+            reject(err)
         } else {
             let employeeArray = results.map((obj) => {
                 return {
-                  value: obj.id,
-                  name: obj.title
+                    //changes keys 'id' & 'title' to 'value' & 'name'
+                    value: obj.id,
+                    name: obj.title
                 };
             });
             // resolve(results)
-            console.log(employeeArray);
-        }
-    })
-    
+            // console.log(employeeArray);
+            //query to get manager name and id
+            db.query("SELECT manager_id, first_name, last_name FROM employees", async (err, results) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    let managerArray = results.map((obj) => {
+                        return {
+                            value: obj.manager_id,
+                            name: obj.first_name + ' ' + obj.last_name,
+                        };
+                    });
 
+                    managerArray.push({ value: 'NULL', name: 'None' });
+                    // console.log(managerArray);
+
+                    await inquirer.prompt([
+                        {
+                            type: 'input',
+                            name: 'firstName',
+                            message: 'What is the employees First Name?',
+                        },
+                        {
+                            type: 'input',
+                            name: 'lastName',
+                            message: 'What is the employees Last Name?',
+                        },
+                        {
+                            type: 'list',
+                            name: 'employeeRole',
+                            message: 'What is the employees role?',
+                            choices: employeeArray,
+                        },
+                        {
+                            type: 'list',
+                            name: 'manager',
+                            message: 'Who is the employees Manager',
+                            choices: managerArray,
+                        },
+                    ])
+                        .then((answers) => {
+                            const first = answers.firstName;
+                            const last = answers.lastName;
+                            const role = answers.employeeRole;
+                            const manager = answers.manager;
+                            console.log(first, last, role, manager);
+                            const addEmployeeQuery = `INSERT INTO employees (first_name, last_name, role_id, manager_id)
+              VALUES ('${first}', '${last}', ${role}, ${manager})`;
+                            db.query(addEmployeeQuery, async function (err, results) {
+                                if (err) {
+                                    console.log(err);
+                                }
+                            });
+                            console.log('');
+                            console.log('EMPLOYEE ADDED'.bold.brightCyan);
+                            console.log('');
+                            initialPrompt();
+                        });
+                }
+            });
+        }
+    });
 }
 
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
-});
+//Update Employee Role
+
+//View All Roles
+const viewAllRoles = async () => {
+    const data = await new Promise((resolve, reject) => {
+        db.query("SELECT id, title, department_id AS 'department', salary FROM roles", (err, results) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(results)
+            }
+        });
+    });
+    console.table(data);
+    initialPrompt();
+};
+
+//Add Roles
+
+//The function will return a Promise that will resolve with the result of the function when it completes.
+//View All Departments
+const viewAllDepartments = async () => {
+    const data = await new Promise((resolve, reject) => {
+        db.query("SELECT id, dep_name AS 'name' FROM department", (err, results) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(results)
+            }
+        });
+    });
+    console.table(data);
+    initialPrompt();
+};
+
+//Add Departments
 
 //prompts application start
 const initialPrompt = () => {
@@ -93,6 +172,7 @@ const initialPrompt = () => {
                     console.log('I chose Update Employee Role');
                     break;
                 case 'View All Roles':
+                    viewAllRoles();
                     console.log('I chose View All Roles');
                     break;
                 case 'Add Role':
@@ -106,6 +186,11 @@ const initialPrompt = () => {
 };
 
 initialPrompt();
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
